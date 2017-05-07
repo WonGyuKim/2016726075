5eeb3cfd9728b8433d8ae61ea5ad1ed5de87f264
WonGyuKim <m55f@naver.com>
2017-05-07 18:55:29 +0900

    new

--- 2016726075_KimWonGyu1.sh
+++ 2016726075_KimWonGyu1.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
-
-echo "===print file information==="
-directory=`pwd`
-
-echo "current directory : $directory"
-numb_line=`ls $directory | wc -l`
-echo "the number of elements : $numb_line"
+##!/bin/bash
+#
+#echo "===print file information==="
+#directory=`pwd`
+#
+#echo "current directory : $directory"
+#numb_line=`ls $directory | wc -l`
+#echo "the number of elements : $numb_line"
+echo "Linux is really really bad guy."
